08582dcd3b543c8ac49d444c7bbe0019ebd1cd3a
hayabaq <33833299+hayabaq@users.noreply.github.com>
2021-05-19 14:27:58 +0300

    Update vulnarability_scanner.py

--- vulnarability_scanner.py
+++ vulnarability_scanner.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import scanner
-target_url = "http://127.0.0.1/dvwa/vulnerabilities/xss_s"
-links_to_ignore = ["http://127.0.0.1/dvwa/logout.php"]
-data_dict = {"username" : "admin" , "password" : "password" , "Login" : "submit"}
+target_url = "http://testphp.vulnweb.com"
+links_to_ignore = ["http://testphp.vulnweb.com/logout.php"]
+data_dict = {"uname" : "test" , "pass" : "test"}
 vuln_scanner = scanner.Scanner(target_url, links_to_ignore)
-vuln_scanner.session.post("http://127.0.0.1/dvwa/login.php" , data=data_dict)
-print(target_url)
+vuln_scanner.session.post("http://testphp.vulnweb.com/login.php" , data=data_dict)
 vuln_scanner.crawl()
 vuln_scanner.run_scanner()
 
