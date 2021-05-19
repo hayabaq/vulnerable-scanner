a4874fd3da6a2aef163127cfa2eae44c3458f2c0
hayabaq <33833299+hayabaq@users.noreply.github.com>
2021-05-19 14:28:36 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,6 +4,7 @@ import re
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
+
 class Scanner:
     def __init__(self, url, ignore_links):
         self.session = requests.Session()
@@ -12,13 +13,24 @@ class Scanner:
         self.links_to_ignore = ignore_links
  
     def extract_links_from(self, url):
-        response = self.session.get(url)
-        x = response.content
-        x= x.decode()
+        #response = self.session.get(url)
+        #x = response.content
+        #x= x.decode(errors= "ignore")
         #print(x)
-        z= re.findall('(?:href=")(.*?)"', x)
+        #z= re.findall('(?:href=")(.*?)"', x)
         #print(z)
-        return z
+        #return z
+        response = self.session.get(url)
+        links=[]
+        parsed_html = BeautifulSoup(response.content.decode(errors= "ignore"),features="html.parser")
+        #print(parsed_html.findAll("a"))
+        for a_tag in parsed_html.findAll("a"):
+            href = a_tag.attrs.get("href")
+            if href == "" or href is None:
+                continue 
+            links.append(href)
+        #print(links)
+        return links
  
     def crawl(self, url=None):
         if url == None:
@@ -31,12 +43,14 @@ class Scanner:
                 link = link.split("#")[0]
             if self.target_url in link and link not in self.target_links:
                 self.target_links.append(link)
-                print(links)
+                print(link)
                 self.crawl(link)
 
     def extract_forms(self, url):
         response = self.session.get(url)
-        parsed_html = BeautifulSoup(response.content,features="html.parser")
+        #response= response.content.decode()
+        parsed_html = BeautifulSoup(response.content.decode(errors= "ignore"),features="html.parser")
+        #print(parsed_html.findAll("form"))
         return parsed_html.findAll("form")
 
     def submit_form(self, form, value, url):
@@ -60,24 +74,29 @@ class Scanner:
         for link in self.target_links:
             forms = self.extract_forms(link)
             for form in forms:
-                print("[+] Testing form in " + link)
+                print("\n\n[+] Testing form in " + link)
                 is_vulnerable_to_xss = self.test_xss_in_form(form, link)
                 if is_vulnerable_to_xss:
-                    print("\n\n[***] XSS discovered in " + link + " in the following form")
+                    print("[***] XSS discovered in " + link + " in the following form")
                     print(form)
+                else:
+                    print("[*] Not Vulnerable to XSS")
+
             if "=" in link:
                 print("\n\n[+] Testing " + link)
                 is_vulnerable_to_xss = self.test_xss_in_link(link)
                 if is_vulnerable_to_xss:
                     print("[***] Discovered XSS in " + link)
+                else:
+                    print("[*] Not Vulnerable to XSS")
         
     def test_xss_in_link(self, url):
         xss_test_script = "<sCript>alert('test)</scriPt>"
         url = url.replace("=", "=" + xss_test_script)
         response = self.session.get(url)
-        return xss_test_script in response.content
+        return xss_test_script in response.content.decode(errors= "ignore")
 
     def test_xss_in_form(self, form, url):
         xss_test_script = "<sCript>alert('test)</scriPt>"
         response = self.submit_form(form, xss_test_script, url)
-        return xss_test_script in response.content
+        return xss_test_script in response.content.decode(errors= "ignore")
